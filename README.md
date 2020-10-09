aa744292de49b1c96a08ed08f027f7d4d811a196
Avishek A <avishek@email.com>
2020-10-09 18:11:44 +0530

    add views, app, form, template, route

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 "# learning_python" 
+
+#django
+> pip3 install Django
+> django-admin startproject PROJECT_NAME
+> python manage.py runserver
+> python manage.py startapp hello
--- cs50_lecture3/hello/views.py
+++ cs50_lecture3/hello/views.py
@@ -1,5 +1,15 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def greet(request, name):
+    #return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render (request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
